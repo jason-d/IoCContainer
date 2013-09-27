b23448851c48f6ee53673f4f5fbe734e4693d010
jason-d <jason.d.ody@gmail.com>
2013-09-27 13:56:15 -0700

    Simple container with tests

---
+++ 1-SimpleContainer/IoCContainer/Container.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IoCContainer._1_SimpleContainer;
+
+namespace IoCContainer._1_SimpleContainer.IoCContainer
+{
+	public class Container
+	{
+		private readonly List<Registration> _registrations;
+
+		public Container()
+		{
+			_registrations = new List<Registration>();
+		}
+
+		public void Register<T, U>()
+			where U : class, new()
+		{
+			var abstractType = typeof(T);
+			var concreteType = typeof(U);
+
+			var registration = new Registration()
+			{
+				AbstractType = abstractType,
+				ConcreteType = concreteType
+			};
+
+			_registrations.Add(registration);
+		}
+
+		public T Resolve<T>()
+			where T : class
+		{
+			var requestedType = typeof(T);
+
+			var registration = _registrations.Where(reg => reg.ConcreteType == requestedType)
+				.FirstOrDefault();
+
+			object instance = null;
+			if (registration != null)
+			{
+				instance = Activator.CreateInstance(registration.ConcreteType);
+			}
+
+			return instance as T;
+		}
+	}
+}
---
+++ 1-SimpleContainer/Tests/ContainerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IoCContainer.TestObjects;
+using IoCContainer._1_SimpleContainer.IoCContainer;
+using NUnit.Framework;
+
+namespace IoCContainer._1_SimpleContainer.Tests
+{
+	[TestFixture]
+	class ContainerTests
+	{
+		[Test]
+		public void Should_return_an_instance_of_a_registered_abstract_type()
+		{
+			var container = new Container();
+			container.Register<ICounter, Counter>();
+
+			var counter = container.Resolve<Counter>();
+
+			Assert.IsInstanceOf<Counter>(counter);
+		}
+
+		[Test]
+		public void Should_return_an_instance_of_a_registered_concrete_type()
+		{
+			var container = new Container();
+			container.Register<Counter, Counter>();
+
+			var counter = container.Resolve<Counter>();
+
+			Assert.IsInstanceOf<Counter>(counter);
+		}
+	}
+}
